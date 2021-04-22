34d53321d2387e1e9000e92052a3709e788140e2
Алексей Горюнов <alexeygorunovpub@gmail.com>
2021-04-23 00:12:10 +0300

    fixs

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -1,11 +1,12 @@
 import os
 from datetime import timedelta
+from django.core.management.utils import get_random_secret_key  
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
 
 DEBUG = False
 
